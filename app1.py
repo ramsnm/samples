9fe2f63349dce3cbbbd273400759a1b0f1c1ee0f
Ramesh Babu <ramesh@rameshs-laptop.lan>
2024-01-24 21:33:58 -0600

    learn py

---
+++ app1.py
@@ -0,0 +1,49 @@
+# print('Hello World')
+# age = 20
+# price = 19.95
+# first_line = "Ramesh is learning"
+# isOnline = True
+# print (age)
+# name = input("What is your name ? ")
+# print ("Hello "+ name)
+# birth_year = input("Enter your year of birth : ")
+# age=2024-int(birth_year)
+# print("Age = " + str(age))
+# first_num = input("First number ")
+# second_num = input("Second number ")
+# print ("Sum is ", int(first_num)+int(second_num))
+
+# course = 'python for beginners'
+# print(course.find('y'))
+# print(course.replace('for', 'your'))
+# print('python' in course)
+
+# print(10**3)
+# x=10
+# # x = x+3
+# x *= 3
+# print(x)
+
+# price = 25
+# print(price>100 or price<30)
+
+# temp = 35
+# if(temp>30):
+#     print("temperate is greater than 30")
+#     print("first block")
+# elif(temp>10):
+#     print("temperatue is greater than 10")
+# else:
+#     print("dont know")
+# print("done")
+
+# weight = input("Whats your weight :")
+# unit = input("(K)g or (L)bs:")
+# if(unit.upper()=="K"):
+#     print("Weight in lbs", float(weight)*2.205)
+# elif(unit.upper()=="L"):
+#     print("Weight in kg", float(weight)/2.205)
+# else:
+#     print("wrong input.. try again")
+
+
